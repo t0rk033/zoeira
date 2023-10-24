85f4a300875cc9c2d7c2da8da52761fa33227830
t0rk033 <115803349+t0rk033@users.noreply.github.com>
2023-10-23 21:31:42 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const seconds = document.querySelector('.seconds')
 const btn = document.querySelector('.btn')
 const popup = document.getElementById('meuPopup');
 const fecharPopup = document.getElementById('fecharPopup');
+const main = document.querySelector('.main-container')
 
 const time = setInterval(function data(){
     let datetoday = new Date();
@@ -16,13 +17,22 @@ const time = setInterval(function data(){
     minutes.textContent = tMinutes
     seconds.textContent = tSeconds
 
-
+    
+    
+    if(tHours >= 18 && tHours <= 23 ){
+        main.style.backgroundImage = 'url(../img/img_noite.jpg)'
+    }
+    else if(tHours >= 12 && tHours < 18){
+        main.style.backgroundImage = 'url(../img/img_tarde.jpg)'
+    }
 })
 
-btn.addEventListener('click', function() {
-  popup.style.display = 'block';
-});
 
-fecharPopup.addEventListener('click', function() {
-  popup.style.display = 'none';
-});
+
+btn.addEventListener('click', function() {
+   popup.style.display = 'block';
+ });
+
+ fecharPopup.addEventListener('click', function() {
+   popup.style.display = 'none';
+ });
